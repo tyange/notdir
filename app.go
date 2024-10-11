3d4e32f265b078a13537baeaf25d3db823263bb0
tyange <usun16@gmail.com>
2024-10-11 21:55:43 +0900

    test multiselect file & open file

--- app.go
+++ app.go
@@ -3,6 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/fs"
+	"os"
+	"os/exec"
+	stdruntime "runtime"
+	"time"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -24,4 +31,65 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+type FileInfo struct {
+	Name    string
+	Size    int64
+	Mode    fs.FileMode
+	ModTime time.Time
+	IsDir   bool
+	Path    string
+}
+
+func (a *App) MultiSelection() ([]FileInfo, error) {
+	paths, err := runtime.OpenMultipleFilesDialog(a.ctx, runtime.OpenDialogOptions{})
+
+	if err != nil {
+		return nil, fmt.Errorf("멀티 파일 선택 중 오류 발생: %v", err)
+	}
+
+	var files []FileInfo
+	for _, value := range paths {
+		fileInfo, err := os.Stat(value)
+
+		if err != nil {
+			return nil, fmt.Errorf("멀티 파일 정보 읽는 중 오류 발생: %v", err)
+		}
+
+		customInfo := FileInfo{
+			Name:    fileInfo.Name(),
+			Size:    fileInfo.Size(),
+			Mode:    fileInfo.Mode(),
+			ModTime: fileInfo.ModTime(),
+			IsDir:   fileInfo.IsDir(),
+			Path:    value,
+		}
+		files = append(files, customInfo)
+	}
+
+	fmt.Println(files)
+
+	return files, nil
+}
+
+func (a *App) FileOpen(filePath string) error {
+	var cmd = exec.Command("open", filePath)
+
+	switch stdruntime.GOOS {
+	case "darwin": // macOS
+		cmd = exec.Command("open", filePath)
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", "", filePath)
+	case "linux":
+		cmd = exec.Command("xdg-open", filePath)
+	default:
+		return fmt.Errorf("지원하지 않는 운영 체제입니다: %s", stdruntime.GOOS)
+
+	}
+
+	// 명령을 실행합니다.
+	err := cmd.Start()
+
+	return err
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
-import { Greet } from "../wailsjs/go/main/App";
+
+import { main } from "../wailsjs/go/models";
+import { MultiSelection, FileOpen } from "../wailsjs/go/main/App";
 
 function App() {
-  const [testText, setTestText] = useState("");
+  const [files, setFiles] = useState<main.FileInfo[]>([]);
 
-  async function greet() {
-    const result = await Greet("notdir");
-    setTestText(result);
+  async function selectFiles(): Promise<void> {
+    const result = await MultiSelection();
+    setFiles(result);
+  }
+
+  async function fileOpenHandler(path: string): Promise<void> {
+    await FileOpen(path);
   }
 
   return (
     <div id="App">
       <p className="text-red-600">notdir</p>
-      <button className="btn btn-primary" onClick={greet}>
-        greet
+      <button className="btn btn-primary" onClick={selectFiles}>
+        Select Files
       </button>
-      <p>{testText}</p>
+      <ul>
+        {files &&
+          files.length > 0 &&
+          files.map((file) => (
+            <li onClick={() => fileOpenHandler(file.Path)}>{file.Name}</li>
+          ))}
+      </ul>
     </div>
   );
 }
