a1cf96f4794f32320a32cc698f6ec244d7b3c217
tyange <usun16@gmail.com>
2024-10-19 10:41:20 +0900

    스타일링 수정

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col w-full h-screen p-5 mt-14">
+    <div className="flex flex-col w-full h-screen p-5 pt-20">
       <main className="flex justify-between flex-1 gap-5">
         <Menu />
         <div className="flex-1 relative">{children}</div>
